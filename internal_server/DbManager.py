86edb2f43f876148b487a673ec93b031b859d16e
Vladimir Nazarov <ruvater@gmail.com>
2016-06-18 21:27:47 +0300

    Fixed bug

--- internal_server/DbManager.py
+++ internal_server/DbManager.py
@@ -65,10 +65,9 @@ class DbManager:
         c.execute("SELECT count(id) FROM archived_chain_info ORDER BY id DESC")
         chain_count = c.execute("SELECT count(id) FROM archived_chain_info ORDER BY id DESC").fetchone()[0]
         chain_id = c.execute("SELECT id FROM archived_chain_info ORDER BY id DESC").fetchone()[0] if chain_count > 0 else 1
-        info = (chain_id, started_at, datetime.now())
 
-        c.execute("ALTER TABLE current_chain RENAME TO ?", ('chain_{}'.format(chain_id),))
-        c.execute("INSERT INTO archived_chain_info(id, started_at, finished_at) VALUES (?,?,?)", (info,))
+        c.execute("ALTER TABLE current_chain RENAME TO chain_{}".format(chain_id))
+        c.execute("INSERT INTO archived_chain_info(id, started_at, finished_at) VALUES ({},{},{})".format(chain_id, started_at, datetime.now()))
 
         self.db_conn.commit()
         self.create_table_current_chain
